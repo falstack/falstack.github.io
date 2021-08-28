3c28a13a8941912427e169c2b772f456c85ba2fe
Sherry Chan <sherrychan@SherrydeMacBook-Pro.local>
2021-08-28 14:44:59 +0800

    feat: add cdn for assets

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,9 +1,15 @@
 import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
+import type { DefaultThemeOptions,  } from 'vuepress'
+
 
 export default defineUserConfig<DefaultThemeOptions>({
     theme: '@calibur/eva',
     title: '{ do something }',
+    bundlerConfig: {
+      chainWebpack: (config) => {
+        config.output.publicPath = '//cdn.jsdelivr.net/gh/falstack/falstack.github.io@gh-pages/'
+      }
+    },
     themeConfig: {
       author: '冰淤', // will display on the page footer
     }
